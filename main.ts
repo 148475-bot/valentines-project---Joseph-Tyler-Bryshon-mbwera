b030f645b56d4d6418816bb0af8a8a8e27068f58
148750-forgotten03 <148750@kvilleps.org>
2026-02-11 08:19:30 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
 
-//this is bryshon
 
 // -------------- DATA STRUCTURES -----------------------
 //creating new types and interfaces is a touch too much 
